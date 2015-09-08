20546940b386adb74c5719bdf7d059026142ef01
Francois Michonneau <francois.michonneau@gmail.com>
2015-09-08 12:34:45 -0400

    first attempt at BEAST 1 submission function

---
+++ tests/testthat/test-cipres_submit_beast1.R
@@ -0,0 +1,10 @@
+context("cipres_submit_beast1 validate input format")
+
+test_that("use_beagle is logical", {
+              infile <- tempfile()
+              cat("test", file = infile)
+              expect_error(cipres_submit_beast1(input_file = infile,
+                                                use_beagle = 1),
+                           "logical")
+              unlink(infile)
+          })
