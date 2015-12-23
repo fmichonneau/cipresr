a418c1c30227c358e28d0450ffce3d0b75e9a269
Francois Michonneau <francois.michonneau@gmail.com>
2015-12-23 11:34:59 +0100

    cleaned up base code

--- R/base.R
+++ R/base.R
@@ -1,12 +1,12 @@
 
 cipres_version <- "v1"
 base_url <- paste("https://cipresrest.sdsc.edu/cipresrest", "v1", sep = "/")
+
+## Variables for location of the appdir directory
 APPNAME <- "cipresr"
 APPAUTHOR <- "fmichonneau"
 
-##' Easy programmatic access to the login information. This function
-##' shouldn't be used by normal users and is only intended to be
-
+## check error codes
 cipres_check <- function(res) {
     if (res$status_code < 400)
         return(invisible())
@@ -17,6 +17,7 @@ cipres_check <- function(res) {
 
 
 cipres_results <- function(res) {
+    cipres_check(res)
     if (identical(res[["headers"]][["content-type"]], "application/xml")) {
         return(xml2::read_xml(httr::content(res, as = "text")))
     } else {
@@ -24,35 +25,45 @@ cipres_results <- function(res) {
     }
 }
 
+cipres_parse <- function(res) {
+    txt <- httr::content(res, as = "text")
+    if (identical(txt, ""))
+        stop("No output to parse", call. = FALSE)
+
+}
+
 cipres_POST <- function(body, ...) {
     res <- httr::POST(url = paste(base_url, "job", cipres_login()$user, sep = "/"),
                       httr::add_headers(`cipres-appkey` = cipres_login()$app_id),
-                      c(#httr::verbose(),
-                        httr::authenticate(user = cipres_login()$user, password = cipres_login()$password)),
+                      c(httr::authenticate(user = cipres_login()$user,
+                                           password = cipres_login()$password)),
                       body = body,
                       encode = "multipart",
                       ...)
-    cipres_check(res)
     cipres_results(res)
 }
 
-cipres_GET <- function(path, ...) {
-    res <- httr::GET(url = paste(base_url, "job", cipres_login()$user, path, sep = "/"),
+cipres_GET <- function(path, full_url, ...) {
+
+    if (missing(full_url)) {
+        url <- paste(base_url, "job", cipres_login()$user, path, sep = "/")
+    } else if (missing(path)) {
+        url <- full_url
+    } else stop("Problem in URL construction")
+
+    res <- httr::GET(url = url,
                      httr::add_headers(`cipres-appkey` = cipres_login()$app_id),
-                     c(#httr::verbose(),
-                       httr::authenticate(user = cipres_login()$user, password = cipres_login()$password))
-                     )
-    cipres_check(res)
+                     c(httr::authenticate(user = cipres_login()$user,
+                                          password = cipres_login()$password)))
     cipres_results(res)
 }
 
 cipres_DELETE <- function(path, ...) {
     res <- httr::DELETE(url = paste(base_url, "job", cipres_login()$user, path, sep = "/"),
                         httr::add_headers(`cipres-appkey` = cipres_login()$app_id),
-                        c(#httr::verbose(),
-                            httr::authenticate(user = cipres_login()$user, password = cipres_login()$password))
+                        c(httr::authenticate(user = cipres_login()$user,
+                                             password = cipres_login()$password))
                         )
-    cipres_check(res)
     cipres_results(res)
 }
 
